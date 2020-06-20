a82ec5c0f6a7245636be9bf55dffd47d796c027a
opera7133 <ainznino@pm.me>
2020-06-20 12:28:31 +0900

    Fadeoutの修正

--- static/js/add-on.js
+++ static/js/add-on.js
@@ -36,6 +36,7 @@ toggle.addEventListener("click", () => {
   } else if (icon_toggle.className === "fas fa-sun fa-lg") {
     setTheme("light");
   }
+  setTimeout('document.body.classList.remove(‘item’);', 800);
 });
 
 function setTheme(mode) {
@@ -48,4 +49,5 @@ function setTheme(mode) {
     icon_toggle.className = "fas fa-moon fa-lg";
     localStorage.setItem("dark-mode-storage", mode);
   }
+  setTimeout('document.body.classList.remove(‘item’);', 800);
 }
